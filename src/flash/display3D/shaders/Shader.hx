c8e7d1c8bb8eaacec5b36881a71782fc07b945eb
Wighawag <wighawag@gmail.com>
2013-07-10 14:12:07 +0530

    fix and move Context3DUtils to openfl-stage3d

--- src/flash/display3D/shaders/Shader.hx
+++ src/flash/display3D/shaders/Shader.hx
@@ -6,10 +6,6 @@ package flash.display3D.shaders;
 
 #if flash
 typedef Shader = flash.utils.ByteArray;
-#elseif (cpp || neko)
-import openfl.gl.GL;
+#elseif (cpp || neko || js)
 typedef Shader = openfl.gl.GLShader;
-#elseif js
-import browser.gl.GL;
-typedef Shader = browser.gl.GLShader;
  #end
